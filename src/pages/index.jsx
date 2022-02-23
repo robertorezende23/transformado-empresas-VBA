d326e0bd0e22ace49f447df5493f7463916fbdfe
robertorezende23 <robertovrd2106@gmail.com>
2022-02-22 22:24:10 -0300

    Alterando subtitulo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ export default function home(){
     return (
         <div > 
         <h1 className={styles.title}>VBA Transformando Empresas</h1>
-        <h2>Vamos entender o poder que essa linguagem ainda possui</h2>
+        <h2>Um repositório de codigos para auxilar no desenvolvimento em VBA</h2>
        <img src="images/capa.png" alt="" />
     
     </div>
