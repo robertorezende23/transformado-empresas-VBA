fb47c6e22d2bdc02dfcceaadc41fb34c85a626a1
robertorezende23 <robertovrd2106@gmail.com>
2022-02-22 22:13:23 -0300

    Css

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-function home(){
-    return <div>Eu te amo um tantão assim ó</div>
-}
-export default home
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import styles from './title.module.css'
+export default function home(){
+    return (
+        <div > 
+        <h1 className={styles.title}>VBA Transformando Empresas</h1>
+        <h2>Vamos entender o poder que essa linguagem ainda possui</h2>
+       <img src="images/capa.png" alt="" />
+    
+    </div>
+    )
+}
